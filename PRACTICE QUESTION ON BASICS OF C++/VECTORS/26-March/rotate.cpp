7bd419b6d9db84f87fc4ffb3d0642df6fb051428
Gaurav Juneja <gjuneja555@gmail.com>
2024-04-18 23:16:33 +0530

    update

--- PRACTICE QUESTION ON BASICS OF C++/VECTORS/26-March/rotate.cpp
+++ PRACTICE QUESTION ON BASICS OF C++/VECTORS/26-March/rotate.cpp
@@ -6,11 +6,9 @@ int main(){
     int k;
     vector <int> v(6);
     v={3,4,5,7,8};
-
+    //trial
     cout<<"Enter the number of steps to rortate"<<endl;
     cin>>k;
-
-
     for(int i=0; i<v.size(); i++){
 
         v[i+k] = v[i];
